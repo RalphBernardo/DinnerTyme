95ba79af5dc3720f2b72c8b2e4620271ba2b2fdc
Ralph Bernardo <ralphbernardo@gmail.com>
2012-11-13 14:39:06 -0500

    carrierwave heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,8 +9,6 @@ CarrierWave.configure do |config|
   
   config.fog_directory = ENV['S3_BUCKET']
 
-  config.s3_access_policy = :public_read  # Generate http:// urls. Defaults to :authenticated_read (https://)
-
   config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET']}"
   
   # use only one of the following 2 settings for cloudfront
