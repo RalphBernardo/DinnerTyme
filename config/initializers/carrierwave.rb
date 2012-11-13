1d92232165c43a6d43c9c3579781264458a0dca5
Ralph Bernardo <ralphbernardo@gmail.com>
2012-11-13 13:19:47 -0500

    carrierwave and S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => "AWS",
-    #:aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    #:aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
   
-  #config.fog_directory = ENV['S3_BUCKET']
+  config.fog_directory = ENV['S3_BUCKET']
 
   # use only one of the following 2 settings
   #config.fog_host = "http://#{ENV['S3_BUCKET']}.s3.amazonaws.com" # for no cloudfront
